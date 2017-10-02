adac9f5a8e901e5b4bb31649fa9ac7f1a97735bf
Matthew Hunger <matt.hunger@gmail.com>
2017-10-02 10:28:30 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   board_piece = 0
   counter = 0
   board.each do
-    board[board_piece] == "X" || "O" ? counter += 1
+    board[board_piece] == "X" || "O" ? counter += 1 : counter += 0
   end
 
 end
